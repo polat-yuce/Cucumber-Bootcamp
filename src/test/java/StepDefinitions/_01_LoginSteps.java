3ad64ddc703251bf3bc76e21276e1c7e79a0c734
Polat <polat.hd7@gmail.comgit config --global user.email polat.hd7@gmail.com>
2025-05-30 23:32:14 +0300

    ParentPage hazırlandı

--- src/test/java/StepDefinitions/_01_LoginSteps.java
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -3,29 +3,37 @@ package StepDefinitions;
 //import io.cucumber.java.en.Given;
 //import io.cucumber.java.en.Then;
 //import io.cucumber.java.en.When;   //Yukarıdaki 3 impotrun yerine aşağıdaki yazılabilir
+import Pages.DialogContent;
 import Utilities.GWD;
 import io.cucumber.java.en.*;
 
 public class _01_LoginSteps {
+    DialogContent dc=new DialogContent();
 
     @Given("Navigate to Campus")
     public void navigate_to_campus() {
         System.out.println("Site açıldı");
-
         GWD.getDriver().get("https://test.mersys.io/");
     }
 
     @When("Enter username and password and click login button")
     public void enter_username_and_password_and_click_login_button() {
         System.out.println("Username ve şifre gönderildi");
-        //Techno2025.%
+
+//        dc.username.sendKeys("turkeyts");
+//        dc.password.sendKeys("Techno2025.%");
+//        dc.loginButton.click();
+
+        dc.mySendKeys(dc.username,"turkeyts");
+        dc.mySendKeys(dc.password,"Techno2025.%");
+        dc.myClick(dc.loginButton);
+
+
     }
 
     @Then("User should login successfully")
     public void user_should_login_successfully() {
         System.out.println("Siteye girildiği doğrulandı");
-
-
     }
 
 
