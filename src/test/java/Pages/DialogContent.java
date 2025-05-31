8cbe239a060ba1023b4278ec847fe130df3c00d7
Polat <polat.hd7@gmail.comgit config --global user.email polat.hd7@gmail.com>
2025-05-31 12:49:11 +0300

    Citizenships soru

--- src/test/java/Pages/DialogContent.java
+++ src/test/java/Pages/DialogContent.java
@@ -1,9 +1,14 @@
 package Pages;
 
 import Utilities.GWD;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 
 public class DialogContent extends ParentPage{
 
@@ -19,4 +24,48 @@ public class DialogContent extends ParentPage{
 
     @FindBy(css = "[aria-label='LOGIN']")
     public WebElement loginButton;
+
+    @FindBy(css="span[class='mat-mdc-tooltip-trigger logo-text']")
+    public WebElement headText;
+
+    @FindBy(xpath="//ms-add-button[contains(@tooltip,'TITLE.ADD')]//button")
+    public WebElement addButton;
+
+    @FindBy(xpath = "//ms-text-field[@formcontrolname='name']//input")
+    public WebElement nameInput;
+
+    @FindBy(xpath = "//ms-text-field[@formcontrolname='code']//input")
+    public WebElement codeInput;
+
+    @FindBy(xpath = "//ms-save-button[@class='ng-star-inserted']//button")
+    public WebElement saveButton;
+
+    @FindBy(xpath = "//div[contains(text(),'successfully')]")
+    public WebElement successMessage;
+
+    @FindBy(xpath = "//ms-text-field[@formcontrolname='shortName' ]//input")
+    public WebElement shortName;
+
+    @FindBy(tagName = "mat-panel-description")
+    public WebElement messageBox;
+
+
+
+
+
+
+
+
+
+
+    
+
+    public void verifyMessageContainsText(String value){
+        wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath("//hot-toast-container/div/div/div//*"),0));
+        Assert.assertTrue( this.messageBox.getAttribute("innerHTML").toLowerCase().contains(value.toLowerCase()));
+
+        //sayfaya ESC tuşu gönderildi, açık mesaj kalmasın diye
+        new Actions(GWD.getDriver()).sendKeys(Keys.ESCAPE).build().perform();
+    }
+
 }
--- src/test/java/StepDefinitions/_01_LoginSteps.java
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -3,12 +3,13 @@ package StepDefinitions;
 //import io.cucumber.java.en.Given;
 //import io.cucumber.java.en.Then;
 //import io.cucumber.java.en.When;   //Yukarıdaki 3 impotrun yerine aşağıdaki yazılabilir
+
 import Pages.DialogContent;
 import Utilities.GWD;
 import io.cucumber.java.en.*;
 
 public class _01_LoginSteps {
-    DialogContent dc=new DialogContent();
+    DialogContent dc = new DialogContent();
 
     @Given("Navigate to Campus")
     public void navigate_to_campus() {
@@ -20,12 +21,13 @@ public class _01_LoginSteps {
     public void enter_username_and_password_and_click_login_button() {
         System.out.println("Username ve şifre gönderildi");
 
+//         Login ekranı gözükene kadar bekle
 //        dc.username.sendKeys("turkeyts");
 //        dc.password.sendKeys("Techno2025.%");
 //        dc.loginButton.click();
 
-        dc.mySendKeys(dc.username,"turkeyts");
-        dc.mySendKeys(dc.password,"Techno2025.%");
+        dc.mySendKeys(dc.username, "turkeyts");
+        dc.mySendKeys(dc.password, "Techno2025.%");
         dc.myClick(dc.loginButton);
 
 
@@ -33,7 +35,14 @@ public class _01_LoginSteps {
 
     @Then("User should login successfully")
     public void user_should_login_successfully() {
-        System.out.println("Siteye girildiği doğrulandı");
+        //System.out.println("Siteye girildiği doğrulandı");
+
+//        WebDriverWait wait=new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(5));
+//        wait.until(ExpectedConditions.textToBePresentInElement(dc.headText,"Internship"));
+//
+//        Assert.assertTrue(dc.headText.getText().equals("Internship") );
+
+        dc.LoginContainsText(dc.headText,"Internship");
     }
 
 
