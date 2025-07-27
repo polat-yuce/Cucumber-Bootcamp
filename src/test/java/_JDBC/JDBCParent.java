f6246af65cc337d39871000693e71e420d47e2c1
Polat <polat.hd7@gmail.comgit config --global user.email polat.hd7@gmail.com>
2025-07-27 14:29:47 +0300

    jdbc bitti

---
+++ src/test/java/_JDBC/JDBCParent.java
@@ -0,0 +1,29 @@
+package _JDBC;
+
+import java.sql.*;
+
+public class JDBCParent {
+    private static Connection connection;
+    public static Statement statement;
+
+    public static void DBConnectionOpen() {
+        String url = "jdbc:mysql://demo.mersys.io:33906/sakila";
+        String username = "admin";
+        String password = "Techno24Study.%=";
+
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+            statement = connection.createStatement();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static void DBConnectionClose() {
+        try {
+            connection.close();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
